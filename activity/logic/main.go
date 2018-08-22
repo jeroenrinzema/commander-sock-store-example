9f0add3671a86bff2e28ec121d78d33844c66030
Jeroen Rinzema <jeroen.rinzema@sysco.no>
2018-08-22 16:39:52 +0200

    feat: activity release

--- activity/logic/main.go
+++ activity/logic/main.go
@@ -3,13 +3,12 @@ package main
 import (
 	"github.com/jeroenrinzema/commander-sock-store-example/activity/logic/common"
 	"github.com/jeroenrinzema/commander-sock-store-example/activity/logic/controllers"
-	"github.com/jeroenrinzema/commander-sock-store-example/cart/logic/common"
 )
 
 func main() {
 	commander := common.OpenCommander()
 
-	commander.NewCommandHandle("ItemSelected", controllers.OnItemSelected)
+	commander.NewCommandHandle("ItemSelected", controllers.OnItemWatched)
 
 	go commander.Consume()
 	commander.CloseOnSIGTERM()
